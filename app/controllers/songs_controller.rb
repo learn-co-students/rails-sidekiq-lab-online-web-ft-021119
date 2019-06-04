4fd0093dd6774abd603beaedb1275cf8a97f013d
Lizzie <lchan217@gmail.com>
2019-06-04 10:05:53 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,9 +1,11 @@
 class SongsController < ApplicationController
+  require 'csv'
   def index
     @songs = Song.all
   end
 
   def upload
+
      SongsWorker.perform_async(params[:file].path)
      redirect_to songs_path
   end
@@ -26,12 +28,12 @@ class SongsController < ApplicationController
     end
   end
 
-  def upload
-    CSV.foreach(params["file"].path, headers: true) do |song|
-      Song.create(title: song[0], artist_name: song[1])
-    end
-    redirect_to songs_path
-  end
+  # def upload
+  #   CSV.foreach(params["file"].path, headers: true) do |song|
+  #     Song.create(title: song[0], artist_name: song[1])
+  #   end
+  #   redirect_to songs_path
+  # end
 
   def edit
     @song = Song.find(params[:id])
